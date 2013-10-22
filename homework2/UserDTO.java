3f622aa46b4fe5c613868c236d7b3b76c7bfae65
Ermakov Petr <ermakov.pd@gmail.com>
2013-10-23 02:09:48 +0400

    homework 2.3
    fhj837hf9376hgf93hf832jf9

--- homework2/UserDTO.java
+++ homework2/UserDTO.java
@@ -43,21 +43,15 @@ public class UserDAO {
 
         String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
 
-        // XXX WORK HERE
-        // create an object suitable for insertion into the user collection
-        // be sure to add username and hashed password to the document. problem instructions
-        // will tell you the schema that the documents must follow.
-
-
+        DBObject user = new BasicDBObject("_id", username)
+                .append("password", passwordHash);
 
         if (email != null && !email.equals("")) {
-            // XXX WORK HERE
-            // if there is an email address specified, add it to the document too.
+            user = ((BasicDBObject) user).append("email", email);
         }
 
         try {
-            // XXX WORK HERE
-            // insert the document into the user collection here
+            usersCollection.insert(user);
             return true;
         } catch (MongoException.DuplicateKey e) {
             System.out.println("Username already in use: " + username);
@@ -68,8 +62,9 @@ public class UserDAO {
     public DBObject validateLogin(String username, String password) {
         DBObject user = null;
 
-        // XXX look in the user collection for a user that has this username
-        // assign the result to the user variable.
+        String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
+
+        user = usersCollection.findOne(new BasicDBObject("_id", username));
 
         if (user == null) {
             System.out.println("User not in database");
